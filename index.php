22bc2db777af13672388a798d76a4238a4feba8b
AdeMitchell <adrian@veeqo.com>
2018-12-10 12:44:49 +0000

    Creating slider

--- index.php
+++ index.php
@@ -30,6 +30,20 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <style>
+        .slider {
+          width: 300px;
+          height: 300px;
+          display: flex;
+          overflow-x: auto;
+        }
+        .slide {
+          width: 300px;
+          flex-shrink: 0;
+          height: 100%;
+        }
+    </style>
 </head>
 
 
